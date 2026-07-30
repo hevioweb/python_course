1ba138ff416c329a11e9bb9a1aa5fdbcbd2d0d61
hevioweb <hevio.web@gmail.com>
2026-07-30 10:07:50 +0200

    started chapter 4, compleated 4.1 and 4.2

---
+++ python-crash-course/Chapter_4_working_with_lists/exercise_batch_1/exercise_animals_4_2.py
@@ -0,0 +1,6 @@
+animals = ["Cat", "Dog", "bird"]
+
+for animal in animals: #here we declare a singular name of the list 
+    print(f"{animal.title()} is great house pet") #as a variable we insert the actual singular variable, not teh list variable
+
+print("\nEach are great\n")
---
+++ python-crash-course/Chapter_4_working_with_lists/exercise_batch_1/exercise_pizzas_4_1.py
@@ -0,0 +1,6 @@
+pizzas = ['pepperoni', 'mushroom', 'green pepper']
+
+for pizza in pizzas:
+    print(f"I like {pizza} pizza")
+
+print("\nI really like pizza\n")
